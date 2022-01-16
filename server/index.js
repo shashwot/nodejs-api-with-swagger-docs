eefc265350ae440c29d7007c8b3d877b944de0c9
shashwot <shashwot@gmail.com>
2022-01-16 21:26:39 +0545

    exception handling

--- server/index.js
+++ server/index.js
@@ -7,4 +7,15 @@ const server = http.createServer(app)
 server.listen(port, () => {
     console.log(`listening on port on ${port} ` + new Date())
 })
+server.on('error', onError);
 
+function onError(error) {
+    if (error.code === 'EADDRINUSE') {
+        console.error( 'Port ' + port + ' is already in use');
+        process.exit(1);
+    }
+    if (error.code === 'EACCES') {
+        console.error( 'Requires elevated privileges');
+        process.exit(1);
+    }
+}
